042e386c7dc4c3e86df917aece85933889cea825
unfav-mibssm <mibsamsaura@gmail.com>
2026-02-21 00:24:21 +0530

    Create words.js

---
+++ words.js
@@ -0,0 +1,7 @@
+// words.js
+const wordBank = [
+    "Apple", "Banana", "Pizza", "Airplane", "Cactus", "Spider", "Guitar", 
+    "Rocket", "Ice Cream", "Bicycle", "Dragon", "Sushi", "Rainbow", 
+    "Fireman", "Penguin", "Octopus", "Pyramid", "Submarine", "Helicopter",
+    "Laptop", "Donut", "Microscope", "Dolphin", "Spaceship", "Cupcake", "Sunflower"
+];
